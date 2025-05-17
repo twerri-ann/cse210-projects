d815d14e9bb0a952d788815647f61786ddbe6898
twerri-ann <terriannbassaragh491@gmail.com>
2025-05-17 15:56:52 -0500

    saved

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -10,13 +10,14 @@ class Program
 
         while (running)
         {
+            Console.WriteLine("Welcome to the Journal Entry Program!");
             Console.WriteLine("\nJournal Menu:");
             Console.WriteLine("1. Write a new entry");
             Console.WriteLine("2. Display journal");
             Console.WriteLine("3. Save journal to file");
             Console.WriteLine("4. Load journal from file");
             Console.WriteLine("5. Quit");
-            Console.Write("Select an option (1-5): ");
+            Console.Write("What would you like to do? (select an option from 1-5) ");
 
             string choice = Console.ReadLine();
             Console.WriteLine();
